14b8308a46670f3cb6a5bde1a6e6404cf2460514
Jung Dettelback <113713179+jdettelback@users.noreply.github.com>
2023-02-16 10:04:17 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,62 @@
+// sk_test_51LnUKJDM1jwCEz8OJG69szv032rIo4X0WrFMaXrqxu9g8fdohsL1y54JEUhFUKrqoBquVjN3AzpIFyrbN915bgcd00O5hqoGCJ
+// Coffee: price_1LnUTFDM1jwCEz8OGoOSXiSM
+// Sunglasses: price_1LnUTxDM1jwCEz8OAqHYTwKQ
+// Camera: price_1LnUUoDM1jwCEz8OvxIcJ7to
+const express = require('express');
+var cors = require('cors');
+const stripe = require('stripe')('sk_test_51LnUKJDM1jwCEz8OJG69szv032rIo4X0WrFMaXrqxu9g8fdohsL1y54JEUhFUKrqoBquVjN3AzpIFyrbN915bgcd00O5hqoGCJ');
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "./store/public")));
+app.use(express.json());
+
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "./store/public/index.html"));
+//});
+
+
+app.post("/checkout", async (req, res) => {
+    /*
+    req.body.items
+    [
+        {
+            id: 1,
+            quantity: 3
+        }
+    ]
+
+    stripe wants
+    [
+        {
+            price: 1,
+            quantity: 3
+        }
+    ]
+    */
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
